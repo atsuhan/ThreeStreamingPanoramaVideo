d6b546c47f650d2d6dd21684df61d73c4233e554
atsuhan <atsuhan220@gmail.com>
2020-07-10 03:18:03 +0900

    feat : add three js base component

---
+++ src/components/three/ThreeBaseComponent.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import * as THREE from 'three';
+
+let renderer: THREE.WebGLRenderer;
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let ambientlLight: THREE.AmbientLight;
+let directionalLight: THREE.DirectionalLight;
+
+const initThreeRenderer = (canvasEl: HTMLCanvasElement): void => {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    canvas: canvasEl,
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+};
+
+const initThreeScene = (): void => {
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x1c1c1c);
+};
+
+const initThreeCamera = (): void => {
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight);
+  camera.position.set(-10, 10, 10);
+  // new THREE.OrbitControls(camera, renderer.domElement);
+};
+
+const initThreeLight = (): void => {
+  // AmbientLight
+  ambientlLight = new THREE.AmbientLight(0xffffff, 1.0);
+  scene.add(ambientlLight);
+
+  // directionalLight
+  directionalLight = new THREE.DirectionalLight('#ffffff', 1.0);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.width = 2048;
+  directionalLight.shadow.mapSize.height = 2048;
+  directionalLight.position.set(10, 10, 10);
+  directionalLight.lookAt(0, 0, 0);
+  directionalLight.shadow.camera.top = 10;
+  directionalLight.shadow.camera.right = 10;
+  directionalLight.shadow.camera.bottom = -10;
+  directionalLight.shadow.camera.left = -10;
+  scene.add(directionalLight);
+};
+
+const update = (): void => {
+  renderer.render(scene, camera);
+  requestAnimationFrame(update);
+};
+
+// ReactComponent
+class ThreeBaseComponent extends React.Component {
+  private canvasEl!: HTMLCanvasElement;
+
+  componentDidMount(): void {
+    initThreeRenderer(this.canvasEl);
+    initThreeScene();
+    initThreeCamera();
+    initThreeLight();
+
+    requestAnimationFrame(update); // loop
+  }
+
+  render(): React.ReactElement {
+    return (
+      <>
+        <canvas
+          width='500px'
+          height='500px'
+          ref={(el: HTMLCanvasElement): void => {
+            this.canvasEl = el;
+          }}
+        />
+      </>
+    );
+  }
+}
+export default ThreeBaseComponent;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
 // Base
 import React from 'react';
 import { SEO } from 'src/components';
+import ThreeBaseComponent from 'src/components/three/ThreeBaseComponent';
 
 // For Page
 import 'src/styles/static-global-webar.scss';
-import ScriptTag from 'src/components/ScriptTag';
-
-const Component: React.FCX = () => (
-  <div>
-    <ScriptTag name='threejs' src='//cdnjs.cloudflare.com/ajax/libs/three.js/110/three.min.js' />
-  </div>
-);
 
 const Element = ({ path }: { path: string }): React.ReactElement => (
   <>
     <SEO title='PanoramaStreaming' pathname={path} />
-    <Component />
+    <ThreeBaseComponent />
   </>
 );
 
